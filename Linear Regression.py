c55571c308ef15328621db12fc0ddf4388e3a8f4
kellen0128 <kellen0128@gmail.com>
2018-04-03 16:34:26 -0400

    Create Linear Regression.py

---
+++ Linear Regression.py
@@ -0,0 +1,8 @@
+### Linear Regression in Python
+##  Simple Linear Regression
+
+# How do we measure institutional differences and economic outcomes?
+
+import pandas as pd
+df1 = pd.read_stata('https://github.com/QuantEcon/QuantEcon.lectures.code/raw/master/ols/maketable1.dta')
+df1.head()
